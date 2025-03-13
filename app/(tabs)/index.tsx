6b6bf65ca1546bd50981d874ec4b89de1ed14bb0
blackholeofuniverse <omb4505@gmail.com>
2025-03-13 17:39:17 +0530

    having some issues

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center"
     >
-      <Text className="text-5xl text-dark-200 font-bold">Welcome everyone!</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
     </View>
   );
 }
